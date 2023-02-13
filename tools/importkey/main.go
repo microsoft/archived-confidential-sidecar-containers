6639dbfcba6a5d3b92f4f93867c8aa0df5d50e26
Stavros Volos <svolos@gmail.com>
2023-02-13 13:05:40 +0000

    Importkey reads PEM file. SKR sidecar response carries a key in JWK format. KeyDerivation metadata passed as a seperate blob

--- tools/importkey/main.go
+++ tools/importkey/main.go
@@ -7,10 +7,12 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"io"
@@ -26,9 +28,10 @@ import (
 )
 
 type importKeyConfig struct {
-	Key      skr.KeyBlob         `json:"key"`
-	Claims   [][]skr.ClaimStruct `json:"claims"`
-	Identity common.Identity     `json:"identity, omitempty"`
+	KeyDerivation skr.KeyDerivationBlob `json:"key_derivation,omitempty"`
+	Key           skr.KeyBlob           `json:"key"`
+	Claims        [][]skr.ClaimStruct   `json:"claims"`
+	Identity      common.Identity       `json:"identity,omitempty"`
 }
 
 type RSAKey struct {
@@ -55,14 +58,16 @@ func main() {
 	// variables declaration
 	var configFile string
 	var keyHexString string
+	var keyRSAPEMFile string
 	var runInsideAzure bool
-	var outputKeyfile bool
+	var outputOctetKeyfile bool
 
 	// flags declaration using flag package
 	flag.StringVar(&configFile, "c", "", "Specify config file to process")
 	flag.StringVar(&keyHexString, "kh", "", "Specify oct key bytes in hexstring [optional]")
+	flag.StringVar(&keyRSAPEMFile, "kp", "", "Specify path to RSA key PEM file [optional]")
 	flag.BoolVar(&runInsideAzure, "a", false, "Run within Azure VM [optional]")
-	flag.BoolVar(&outputKeyfile, "out", false, "Output oct key binary file")
+	flag.BoolVar(&outputOctetKeyfile, "out", false, "Output octet key binary file")
 	flag.Parse() // after declaring flags we need to call it
 
 	flag.Parse()
@@ -118,46 +123,119 @@ func main() {
 	var octKey []byte
 
 	if importKeyCfg.Key.KTY == "RSA-HSM" {
-		privateRSAKey, err := rsa.GenerateKey(rand.Reader, 2048)
-		if err != nil {
-			fmt.Println(err)
-			return
+		var jwKey jwk.RSAPrivateKey
+		if keyRSAPEMFile == "" {
+			privateRSAKey, err := rsa.GenerateKey(rand.Reader, skr.RSASize)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			privateRSAKeyBytes, err := x509.MarshalPKCS8PrivateKey(privateRSAKey)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var privateRSAKeyBlock = &pem.Block{
+				Type:  "PRIVATE KEY",
+				Bytes: privateRSAKeyBytes,
+			}
+
+			privateRSAKeyFile, err := os.OpenFile("private_key.pem", os.O_WRONLY|os.O_CREATE, 0644)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			err = pem.Encode(privateRSAKeyFile, privateRSAKeyBlock)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			privateRSAKeyFile.Close()
+
+			// convert to JSON Web Key (JWK) format
+			jwKey = jwk.NewRSAPrivateKey()
+			err = jwKey.FromRaw(privateRSAKey)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// note that if KeyOps is nil, the HSM will whitelist all operations
+			jwKey.Set("key_ops", importKeyCfg.Key.KeyOps)
+			key = jwKey
+		} else {
+
+			privateRSAKeyBytes, err := ioutil.ReadFile(keyRSAPEMFile)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			data, _ := pem.Decode(privateRSAKeyBytes)
+			privateKey, err := x509.ParsePKCS8PrivateKey(data.Bytes)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// convert to JSON Web Key (JWK) format
+			jwKey = jwk.NewRSAPrivateKey()
+			err = jwKey.FromRaw(privateKey.(*rsa.PrivateKey))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			key = jwKey
 		}
+		// if the user specified outputing an octek key binary file, derive a key from the RSA
+		// private key. Output the salt and the label so that the key can be re-derived by
+		// entities in possesion of the private key.
+		//
+		// note that using a derived octet key is safe as long as the RSA key is not used in
+		// other means.
+		if outputOctetKeyfile {
+			// use sha256 as hashing function for HKDF
+			hash := sha256.New
 
-		// convert to JSON Web Key (JWK) format
-		jwKey := jwk.NewRSAPrivateKey()
-		err = jwKey.FromRaw(privateRSAKey)
+			// public salt and label
+			salt := make([]byte, hash().Size())
+			var err error
+			if importKeyCfg.KeyDerivation.Salt != "" {
+				salt, err = hex.DecodeString(importKeyCfg.KeyDerivation.Salt)
+			} else {
+				_, err = rand.Read(salt)
+			}
 
-		if err != nil {
-			fmt.Println(err)
-			return
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var labelString string
+			if importKeyCfg.KeyDerivation.Label != "" {
+				labelString = importKeyCfg.KeyDerivation.Label
+			} else {
+				labelString = "Symmetric Encryption Key"
+			}
+
+			hkdf := hkdf.New(hash, jwKey.D(), salt, []byte(labelString))
+
+			// derive key
+			octKey = make([]byte, 32)
+			if _, err := io.ReadFull(hkdf, octKey); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Printf("Symmetric key %s (salt: %s label: %s)\n", hex.EncodeToString(octKey), hex.EncodeToString(salt), labelString)
 		}
-
-		// use sha256 as hashing function for HKDF
-		hash := sha256.New
-
-		// public salt and label
-		label := []byte("Symmetric Encryption Key")
-		salt := make([]byte, hash().Size())
-		if _, err := rand.Read(salt); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		hkdf := hkdf.New(hash, jwKey.D(), salt, label)
-
-		// derive key
-		octKey = make([]byte, 32)
-		if _, err := io.ReadFull(hkdf, octKey); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Printf("Symmetric key %s (salt: %s label: %s)\n", hex.EncodeToString(octKey), hex.EncodeToString(salt), label)
-
-		jwKey.Set("key_ops", "encrypt")
-		key = jwKey
-	} else if importKeyCfg.Key.KTY == "" {
+	} else if importKeyCfg.Key.KTY == "oct-HSM" || importKeyCfg.Key.KTY == "" {
 		// if not specified, default is to generate an OCT key
 
 		var err error
@@ -175,9 +253,10 @@ func main() {
 
 		fmt.Printf("Symmetric key %s\n", hex.EncodeToString(octKey))
 
+		// note that if KeyOps is nil, the HSM will whitelist all operations
 		key = OctKey{
 			KTY:     "oct-HSM",
-			KeyOps:  []string{"encrypt", "decrypt"},
+			KeyOps:  importKeyCfg.Key.KeyOps,
 			K:       base64.RawURLEncoding.EncodeToString(octKey),
 			KeySize: len(octKey) * 8,
 		}
@@ -186,7 +265,7 @@ func main() {
 		return
 	}
 
-	if outputKeyfile {
+	if outputOctetKeyfile {
 		keyfile, err := os.Create("keyfile.bin")
 		if err != nil {
 			fmt.Println(err)
